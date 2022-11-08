cf70b89fa854b6e62998b28fe7422cb7bce7f8f0
Sam Adams <sam.adams7785@gmail.com>
2022-11-08 14:41:05 -0800

    16 test passing

--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,22 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
-    
+SELECT
+    film.title,
+    film.description,
+    film.release_year,
+    COALESCE (
+        json_agg(to_jsonb(actor)) FILTER (
+            WHERE
+                actor IS NOT NULL
+        )
+    ) as actors
+FROM
+    film
+    LEFT JOIN film_actor on film.film_id = film_actor.film_id
+    LEFT JOIN actor on film_actor.actor_id = actor.actor_id
+WHERE
+    film.title = 'Airplane Sierra'
+GROUP by
+    film.title,
+    film.description,
+    film.release_year
